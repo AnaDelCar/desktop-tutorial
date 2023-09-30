648c40c84a97783b703e2a3a98b1688745633651
AnaDelCar <145411148+AnaDelCar@users.noreply.github.com>
2023-09-29 22:52:52 -0600

    carpeta

---
+++ Taller_Desarrollo/PruebasUnitarias.py
@@ -0,0 +1,12 @@
+
+import math as mt
+
+def evaluacion(a,b):
+    return a*b + a*b
+
+assert(evaluacion(5,4)==40)
+
+def evaluacion2(a,b,c):
+    return (mt.sqrt(a)*b)/c
+
+assert(evaluacion2(25,10,5)==10)
