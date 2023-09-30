bbb50b45d3b0d7f18e8f01dc2c9dead14e41c37c
AnaDelCar <145411148+AnaDelCar@users.noreply.github.com>
2023-09-29 23:22:11 -0600

    Delete Taller_Desarrollo directory

--- Taller_Desarrollo/Medias.py
+++
@@ -1,6 +0,0 @@
-def calcula_media(*args):
-    return sum(*args)/len(*args)
-print(calcula_media([3,7,5 ]))
-
-assert(calcula_media([3,7,5]) == 6.0, "El resultado")
-assert(calcula_media([3,7,5]) == 15.0, "El resultado")
