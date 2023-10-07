1d001b28221f5a6e7d8071569cd3e7af52ea4157
AnaDelCar <otakuchan077@gmail.com>
2023-10-07 12:05:59 -0600

    Formulario de Registro Validando Datos

---
+++ Practica_Validar Datos(Formulario)/Practica_Validar Datos(Formulario)/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Practica_Validar_Datos_Formulario_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_nombre = new System.Windows.Forms.Label();
+            this.label_telefono = new System.Windows.Forms.Label();
+            this.label_edad = new System.Windows.Forms.Label();
+            this.label_estatura = new System.Windows.Forms.Label();
+            this.txt_Nombre = new System.Windows.Forms.TextBox();
+            this.txt_Apellido = new System.Windows.Forms.TextBox();
+            this.txt_Telefono = new System.Windows.Forms.TextBox();
+            this.txt_Estatura = new System.Windows.Forms.TextBox();
+            this.txt_Edad = new System.Windows.Forms.TextBox();
+            this.RadButton_Hombre = new System.Windows.Forms.RadioButton();
+            this.RadButton_Mujer = new System.Windows.Forms.RadioButton();
+            this.label_genero = new System.Windows.Forms.Label();
+            this.label_apellido = new System.Windows.Forms.Label();
+            this.Boton_Cancelar = new System.Windows.Forms.Button();
+            this.Boton_Guardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_nombre
+            // 
+            this.label_nombre.AutoSize = true;
+            this.label_nombre.Location = new System.Drawing.Point(119, 40);
+            this.label_nombre.Name = "label_nombre";
+            this.label_nombre.Size = new System.Drawing.Size(47, 13);
+            this.label_nombre.TabIndex = 0;
+            this.label_nombre.Text = "Nombre:";
+            // 
+            // label_telefono
+            // 
+            this.label_telefono.AutoSize = true;
+            this.label_telefono.Location = new System.Drawing.Point(119, 152);
+            this.label_telefono.Name = "label_telefono";
+            this.label_telefono.Size = new System.Drawing.Size(52, 13);
+            this.label_telefono.TabIndex = 2;
+            this.label_telefono.Text = "Telefono:";
+            // 
+            // label_edad
+            // 
+            this.label_edad.AutoSize = true;
+            this.label_edad.Location = new System.Drawing.Point(119, 271);
+            this.label_edad.Name = "label_edad";
+            this.label_edad.Size = new System.Drawing.Size(35, 13);
+            this.label_edad.TabIndex = 3;
+            this.label_edad.Text = "Edad:";
+            // 
+            // label_estatura
+            // 
+            this.label_estatura.AutoSize = true;
+            this.label_estatura.Location = new System.Drawing.Point(119, 207);
+            this.label_estatura.Name = "label_estatura";
+            this.label_estatura.Size = new System.Drawing.Size(49, 13);
+            this.label_estatura.TabIndex = 4;
+            this.label_estatura.Text = "Estatura:";
+            // 
+            // txt_Nombre
+            // 
+            this.txt_Nombre.Location = new System.Drawing.Point(259, 40);
+            this.txt_Nombre.Name = "txt_Nombre";
+            this.txt_Nombre.Size = new System.Drawing.Size(100, 20);
+            this.txt_Nombre.TabIndex = 5;
+            // 
+            // txt_Apellido
+            // 
+            this.txt_Apellido.Location = new System.Drawing.Point(259, 92);
+            this.txt_Apellido.Name = "txt_Apellido";
+            this.txt_Apellido.Size = new System.Drawing.Size(100, 20);
+            this.txt_Apellido.TabIndex = 6;
+            // 
+            // txt_Telefono
+            // 
+            this.txt_Telefono.Location = new System.Drawing.Point(259, 144);
+            this.txt_Telefono.Name = "txt_Telefono";
+            this.txt_Telefono.Size = new System.Drawing.Size(100, 20);
+            this.txt_Telefono.TabIndex = 7;
+            // 
+            // txt_Estatura
+            // 
+            this.txt_Estatura.Location = new System.Drawing.Point(259, 199);
+            this.txt_Estatura.Name = "txt_Estatura";
+            this.txt_Estatura.Size = new System.Drawing.Size(100, 20);
+            this.txt_Estatura.TabIndex = 8;
+            // 
+            // txt_Edad
+            // 
+            this.txt_Edad.Location = new System.Drawing.Point(259, 264);
+            this.txt_Edad.Name = "txt_Edad";
+            this.txt_Edad.Size = new System.Drawing.Size(100, 20);
+            this.txt_Edad.TabIndex = 9;
+            // 
+            // RadButton_Hombre
+            // 
+            this.RadButton_Hombre.AutoSize = true;
+            this.RadButton_Hombre.Location = new System.Drawing.Point(485, 92);
+            this.RadButton_Hombre.Name = "RadButton_Hombre";
+            this.RadButton_Hombre.Size = new System.Drawing.Size(62, 17);
+            this.RadButton_Hombre.TabIndex = 10;
+            this.RadButton_Hombre.TabStop = true;
+            this.RadButton_Hombre.Text = "Hombre";
+            this.RadButton_Hombre.UseVisualStyleBackColor = true;
+            // 
+            // RadButton_Mujer
+            // 
+            this.RadButton_Mujer.AutoSize = true;
+            this.RadButton_Mujer.Location = new System.Drawing.Point(626, 92);
+            this.RadButton_Mujer.Name = "RadButton_Mujer";
+            this.RadButton_Mujer.Size = new System.Drawing.Size(51, 17);
+            this.RadButton_Mujer.TabIndex = 11;
+            this.RadButton_Mujer.TabStop = true;
+            this.RadButton_Mujer.Text = "Mujer";
+            this.RadButton_Mujer.UseVisualStyleBackColor = true;
+            // 
+            // label_genero
+            // 
+            this.label_genero.AutoSize = true;
+            this.label_genero.Location = new System.Drawing.Point(485, 73);
+            this.label_genero.Name = "label_genero";
+            this.label_genero.Size = new System.Drawing.Size(42, 13);
+            this.label_genero.TabIndex = 12;
+            this.label_genero.Text = "Genero";
+            // 
+            // label_apellido
+            // 
+            this.label_apellido.AutoSize = true;
+            this.label_apellido.Location = new System.Drawing.Point(119, 94);
+            this.label_apellido.Name = "label_apellido";
+            this.label_apellido.Size = new System.Drawing.Size(50, 13);
+            this.label_apellido.TabIndex = 15;
+            this.label_apellido.Text = "Apellido: ";
+            // 
+            // Boton_Cancelar
+            // 
+            this.Boton_Cancelar.Location = new System.Drawing.Point(524, 282);
+            this.Boton_Cancelar.Name = "Boton_Cancelar";
+            this.Boton_Cancelar.Size = new System.Drawing.Size(154, 74);
+            this.Boton_Cancelar.TabIndex = 16;
+            this.Boton_Cancelar.Text = "Cancelar";
+            this.Boton_Cancelar.UseVisualStyleBackColor = true;
+            this.Boton_Cancelar.Click += new System.EventHandler(this.Boton_Cancelar_Click_1);
+            // 
+            // Boton_Guardar
+            // 
+            this.Boton_Guardar.Location = new System.Drawing.Point(524, 183);
+            this.Boton_Guardar.Name = "Boton_Guardar";
+            this.Boton_Guardar.Size = new System.Drawing.Size(154, 61);
+            this.Boton_Guardar.TabIndex = 17;
+            this.Boton_Guardar.Text = "Guardar";
+            this.Boton_Guardar.UseVisualStyleBackColor = true;
+            this.Boton_Guardar.Click += new System.EventHandler(this.Boton_Guardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Boton_Guardar);
+            this.Controls.Add(this.Boton_Cancelar);
+            this.Controls.Add(this.label_apellido);
+            this.Controls.Add(this.label_genero);
+            this.Controls.Add(this.RadButton_Mujer);
+            this.Controls.Add(this.RadButton_Hombre);
+            this.Controls.Add(this.txt_Edad);
+            this.Controls.Add(this.txt_Estatura);
+            this.Controls.Add(this.txt_Telefono);
+            this.Controls.Add(this.txt_Apellido);
+            this.Controls.Add(this.txt_Nombre);
+            this.Controls.Add(this.label_estatura);
+            this.Controls.Add(this.label_edad);
+            this.Controls.Add(this.label_telefono);
+            this.Controls.Add(this.label_nombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_nombre;
+        private System.Windows.Forms.Label label_telefono;
+        private System.Windows.Forms.Label label_edad;
+        private System.Windows.Forms.Label label_estatura;
+        private System.Windows.Forms.TextBox txt_Nombre;
+        private System.Windows.Forms.TextBox txt_Apellido;
+        private System.Windows.Forms.TextBox txt_Telefono;
+        private System.Windows.Forms.TextBox txt_Estatura;
+        private System.Windows.Forms.TextBox txt_Edad;
+        private System.Windows.Forms.RadioButton RadButton_Hombre;
+        private System.Windows.Forms.RadioButton RadButton_Mujer;
+        private System.Windows.Forms.Label label_genero;
+        private System.Windows.Forms.Label label_apellido;
+        private System.Windows.Forms.Button Boton_Cancelar;
+        private System.Windows.Forms.Button Boton_Guardar;
+    }
+}
+
---
+++ Practica_Validar Datos(Formulario)/Practica_Validar Datos(Formulario)/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_Validar_Datos_Formulario_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txt_Nombre.TextChanged += validarNombre;
+            txt_Apellido.TextChanged += validarApellido;
+            txt_Edad.TextChanged += validarEdad;
+            txt_Estatura.TextChanged += validarEstatura;
+            txt_Telefono.TextChanged += validarTelefono;
+        }
+
+        private void Boton_Cancelar_Click_1(object sender, EventArgs e)
+        {
+            txt_Nombre.Clear();
+            txt_Apellido.Clear();
+            txt_Telefono.Clear();
+            txt_Edad.Clear();
+            txt_Estatura.Clear();
+            RadButton_Hombre.Checked = false;
+            RadButton_Mujer.Checked = false;
+        }
+
+        private void Boton_Guardar_Click(object sender, EventArgs e)
+        {
+            //GUARDAR LOS VALORES DE LOS TEXTBOX EN VARIABLES 
+            string nombre = txt_Nombre.Text;
+            string apellido = txt_Apellido.Text;
+            string edad = txt_Edad.Text;
+            string estatura = txt_Estatura.Text;
+            string telefono = txt_Telefono.Text;
+
+            //GUARDAR LA INFO DE LOS RADIOBUTTONS EN VARIABLES
+            string genero = "";
+            if (RadButton_Hombre.Checked)
+            {
+                genero = "Hombre";
+            }
+            else if (RadButton_Mujer.Checked)
+            {
+                genero = "Mujer";
+            }
+            if (EsEnteroValido(edad) && EsDecimalValido(estatura) && EsEnteroValidode10Digitos(telefono) && EsTextoValido(nombre) && EsTextoValido(apellido))
+            {
+                string informacion = $"NOMBRE: {nombre}\r\nAPELLIDO: {apellido}\r\nTELEFONO: {telefono} kg\r\nESTATURA: {estatura} cm\r\nEDAD: {edad} años\r\nGENERO: {genero}";
+
+                string rutaArchivo = "C:/Users/otaku/Downloads/DocumFormulario.txt";
+
+                bool archivoExiste = File.Exists(rutaArchivo);
+                Console.WriteLine(archivoExiste);
+
+                if (archivoExiste == false) //CONDICIONALES PARA QUE GUARDE MULTIPLES DATOS EN EL ARCHIVO
+                {
+                    File.WriteAllText(rutaArchivo, informacion);
+                }
+                else
+                {
+                    using (StreamWriter writer = new StreamWriter(rutaArchivo, true)) //GUARDA LOS DATOS
+                    {
+                        if (archivoExiste)
+                        {
+                            writer.WriteLine("\n");
+                        }
+                        writer.WriteLine(informacion);
+                    }
+                }
+                MessageBox.Show("GUARDADO CON EXITO!:\n\n" + informacion, "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("PORFAVOR INGRESE INFORMACION EN LOS CAMPOS", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private bool EsEnteroValido(string valor)
+        {
+            int resultado;
+            return int.TryParse(valor, out resultado);
+        }
+        private bool EsDecimalValido(string valor)
+        {
+            decimal resultado;
+            return decimal.TryParse(valor, out resultado);
+        }
+        private bool EsEnteroValidode10Digitos(string valor)
+        {
+            long resultado;
+            return long.TryParse(valor, out resultado) && valor.Length == 10;
+        }
+        private bool EsTextoValido(string valor)
+        {
+            return Regex.IsMatch(valor, @"^[a-zA-Z\s]+$");
+        }
+
+        private void validarEdad(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsEnteroValido(textBox.Text))
+            {
+                MessageBox.Show("Porfavor ingrese un valor valido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear(); 
+            }
+        }
+        private void validarEstatura(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsDecimalValido(textBox.Text))
+            {
+                MessageBox.Show("Porfavor ingrese un valor valido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+        private void validarTelefono(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string input = textBox.Text;
+
+            if (input.Length > 10)
+            {
+                if (!EsEnteroValidode10Digitos(input))
+                {
+                    MessageBox.Show("Porfavor ingrese un valor valido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox.Clear();
+                }
+                //} else if (!EsEnteroValidode10Digitos(input))
+                //{
+                //   MessageBox.Show("Porfavor ingrese un valor valido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void validarNombre(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+                if (!EsTextoValido(textBox.Text))
+            {
+                MessageBox.Show("Porfavor ingrese un valor valido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+        private void validarApellido(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (!EsTextoValido(textBox.Text))
+            {
+                MessageBox.Show("Porfavor ingrese un valor valido.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox.Clear();
+            }
+        }
+    }
+}
