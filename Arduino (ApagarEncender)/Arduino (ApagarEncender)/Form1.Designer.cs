fbd26fea412485dfa41f6975a4c18fa80e1f029e
AnaDelCar <otakuchan077@gmail.com>
2023-10-18 12:47:37 -0600

    Cambios

---
+++ Arduino (ApagarEncender)/Arduino (ApagarEncender)/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Arduino__ApagarEncender_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botonApagar = new System.Windows.Forms.Button();
+            this.botonEncender = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // botonApagar
+            // 
+            this.botonApagar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.botonApagar.Font = new System.Drawing.Font("Segoe Script", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonApagar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.botonApagar.Location = new System.Drawing.Point(314, 60);
+            this.botonApagar.Name = "botonApagar";
+            this.botonApagar.Size = new System.Drawing.Size(192, 62);
+            this.botonApagar.TabIndex = 0;
+            this.botonApagar.Text = "Apagar";
+            this.botonApagar.UseVisualStyleBackColor = false;
+            this.botonApagar.Click += new System.EventHandler(this.botonApagar_Click);
+            // 
+            // botonEncender
+            // 
+            this.botonEncender.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.botonEncender.Font = new System.Drawing.Font("Segoe Script", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonEncender.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.botonEncender.Location = new System.Drawing.Point(323, 194);
+            this.botonEncender.Name = "botonEncender";
+            this.botonEncender.Size = new System.Drawing.Size(192, 62);
+            this.botonEncender.TabIndex = 1;
+            this.botonEncender.Text = "Encender";
+            this.botonEncender.UseVisualStyleBackColor = false;
+            this.botonEncender.Click += new System.EventHandler(this.botonEncender_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.botonEncender);
+            this.Controls.Add(this.botonApagar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botonApagar;
+        private System.Windows.Forms.Button botonEncender;
+    }
+}
+
---
+++ Arduino (ApagarEncender)/Arduino (ApagarEncender)/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arduino__ApagarEncender_
+{
+    public partial class Form1 : Form
+    {
+        public SerialPort ArduinoPort { get;  } 
+        public Form1()
+        {
+            InitializeComponent();
+            //CREACION "SERIAL PORT"
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM3"; //DEPENDIENDO DEL ARDUIDO
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.Open();
+
+            //VINCULACION DE EVENTOS
+            this.FormClosing += CerrandoForm1;
+            this.botonApagar.Click += botonApagar_Click;
+            this.botonEncender.Click += botonEncender_Click;
+        }
+
+
+        private void botonApagar_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("a");
+        }
+
+        private void botonEncender_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("b");
+        }
+
+        private void CerrandoForm1 (object sender, FormClosingEventArgs e)
+        {
+            if (ArduinoPort.IsOpen) ArduinoPort.Close();
+        }
+    }
+}
