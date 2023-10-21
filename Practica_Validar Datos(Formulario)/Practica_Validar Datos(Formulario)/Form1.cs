b7db1ba765ab604dce2c71b72dd82985deba5db2
AnaDelCar <otakuchan077@gmail.com>
2023-10-20 18:40:52 -0600

    Formulario_BD

--- Practica_Validar Datos(Formulario)/Practica_Validar Datos(Formulario)/Form1.cs
+++ Practica_Validar Datos(Formulario)/Practica_Validar Datos(Formulario)/Form1.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient; //LIBRERIA DE CONEXION
 
 namespace Practica_Validar_Datos_Formulario_
 {
