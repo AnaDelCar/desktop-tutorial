d0e9ad9dd8355125c3c03d592ee16ab248a7e376
AnaDelCar <otakuchan077@gmail.com>
2023-10-29 16:42:42 -0600

    Listo!

---
+++ SensorTemperatura/SensorTemperatura/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace SensorTemperatura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botConectar = new System.Windows.Forms.Button();
+            this.botDesconec = new System.Windows.Forms.Button();
+            this.labTemperatura = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labConexion = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLimTem = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // botConectar
+            // 
+            this.botConectar.BackColor = System.Drawing.Color.Gold;
+            this.botConectar.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botConectar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.botConectar.Location = new System.Drawing.Point(117, 339);
+            this.botConectar.Name = "botConectar";
+            this.botConectar.Size = new System.Drawing.Size(159, 62);
+            this.botConectar.TabIndex = 0;
+            this.botConectar.Text = "CONECTAR";
+            this.botConectar.UseVisualStyleBackColor = false;
+            this.botConectar.Click += new System.EventHandler(this.botConectar_Click);
+            // 
+            // botDesconec
+            // 
+            this.botDesconec.BackColor = System.Drawing.Color.Goldenrod;
+            this.botDesconec.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botDesconec.Location = new System.Drawing.Point(530, 339);
+            this.botDesconec.Name = "botDesconec";
+            this.botDesconec.Size = new System.Drawing.Size(183, 62);
+            this.botDesconec.TabIndex = 1;
+            this.botDesconec.Text = "DESCONECTAR";
+            this.botDesconec.UseVisualStyleBackColor = false;
+            this.botDesconec.Click += new System.EventHandler(this.botDesconec_Click);
+            // 
+            // labTemperatura
+            // 
+            this.labTemperatura.AutoSize = true;
+            this.labTemperatura.Font = new System.Drawing.Font("Sitka Small", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labTemperatura.Location = new System.Drawing.Point(560, 209);
+            this.labTemperatura.Name = "labTemperatura";
+            this.labTemperatura.Size = new System.Drawing.Size(88, 40);
+            this.labTemperatura.TabIndex = 2;
+            this.labTemperatura.Text = "temp";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(525, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 26);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "TEMPERATURA";
+            // 
+            // labConexion
+            // 
+            this.labConexion.AutoSize = true;
+            this.labConexion.Font = new System.Drawing.Font("Sitka Small", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labConexion.Location = new System.Drawing.Point(114, 209);
+            this.labConexion.Name = "labConexion";
+            this.labConexion.Size = new System.Drawing.Size(93, 19);
+            this.labConexion.TabIndex = 4;
+            this.labConexion.Text = "labConexion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(61, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "LIMTE DE TEMPERATURA";
+            // 
+            // txtLimTem
+            // 
+            this.txtLimTem.Location = new System.Drawing.Point(117, 83);
+            this.txtLimTem.Name = "txtLimTem";
+            this.txtLimTem.Size = new System.Drawing.Size(100, 20);
+            this.txtLimTem.TabIndex = 6;
+            this.txtLimTem.Text = "0";
+            this.txtLimTem.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtLimTem);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labConexion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labTemperatura);
+            this.Controls.Add(this.botDesconec);
+            this.Controls.Add(this.botConectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botConectar;
+        private System.Windows.Forms.Button botDesconec;
+        private System.Windows.Forms.Label labTemperatura;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labConexion;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLimTem;
+    }
+}
+
---
+++ SensorTemperatura/SensorTemperatura/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SensorTemperatura
+{
+    public partial class Form1 : Form
+    {
+        delegate void SetTextDelegate(string value);
+        public SerialPort ArduinoPort
+        {
+            get;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM4"; //DEPENDE DEL EQUIPO
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.DataBits = 8;
+            ArduinoPort.ReadTimeout = 500;
+            ArduinoPort.WriteTimeout = 500;
+            ArduinoPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            //VINCULACCION DE EVENTOS
+            this.botConectar.Click += botConectar_Click;
+        }
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            string dato = ArduinoPort.ReadLine();
+            EscribirTXT(dato);
+        }
+        private void EscribirTXT(string dato)
+        {
+            if (InvokeRequired)
+                try
+                {
+                    Invoke(new SetTextDelegate(EscribirTXT), dato);
+                }
+                catch { }
+            else
+                labTemperatura.Text = dato;
+        }
+        private void botConectar_Click(object sender, EventArgs e)
+        {
+            botDesconec.Enabled = true;
+            try
+            {
+                if (!ArduinoPort.IsOpen)
+                    ArduinoPort.Open();
+                if (int.TryParse(txtLimTem.Text, out int temperatureLimit))
+                {
+                    string limitString = temperatureLimit.ToString();
+                    ArduinoPort.Write(limitString);
+                }
+                else
+                {
+                    MessageBox.Show("Ingresa un valor valido en el 'LIMITE DE TEMPERATURA' ");
+                }
+                labConexion.Text = "CONEXION OKE";
+                labConexion.ForeColor = System.Drawing.Color.DarkBlue;
+            }
+            catch
+            {
+                MessageBox.Show("Configure el puerto de comunicacion correcto o DESCONECTE");
+            }
+    
+            }
+        private void botDesconec_Click(object sender, EventArgs e)
+        {
+            botConectar.Enabled = true;
+            botConectar.Enabled = false;
+            if (ArduinoPort.IsOpen)
+                ArduinoPort.Close();
+            labConexion.Text = "DESCONECTADO";
+            labConexion.ForeColor = System.Drawing.Color.Purple;
+            labTemperatura.Text = "00.0";
+        }
+
+                    
+    }
+    }
+
